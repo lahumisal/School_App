6bc07ae47833cbf58091e3fdaa335cd5da65f1fe
lahumisal <lahumisal402@gmail.com>
2026-01-24 18:40:32 +0530

    added fields message error class

---
+++ src/main/java/com/smart_school/smart_school_management/exceptions/validation/FieldErrorMessages.java
@@ -0,0 +1,28 @@
+package com.smart_school.smart_school_management.exceptions.validation;
+
+public class FieldErrorMessages {
+
+    public static final  String parentId = "parentId";
+    public static final String childId = "childId";
+    public static final String parentName = "parentName";
+    public static final String childName = "childName";
+    public static final String FIRST_NAME_NOT_BLANK = "firstName not be empty : firstName";
+    public static final String LAST_NAME_NOT_BLANK = "lastName not be empty : lastName";
+    public static final String FIRSTNAME = "firstName";
+    public static final String LASTNAME = "lastName";
+    public static final String MIDDLE_NAME_NOT_BLANK = "middleName not be empty : middleName";
+
+    public static final String EMAIL_REQUIRED = "Email must not be blank";
+    public static final String EMAIL_INVALID = "Invalid email format";
+
+    public static final String DISTRICT_REQUIRED = "District must not be blank";
+    public static final String STATE_REQUIRED = "State must not be blank";
+    public static final String CITY_REQUIRED = "City must not be blank";
+    public static final String COUNTRY_REQUIRED = "Country must not be blank";
+    public static final String ADDRESS_REQUIRED = "Address must not be blank";
+
+    public static final String CASTE_REQUIRED = "Caste must not be blank";
+
+    public static final String CONTACT_REQUIRED = "Contact number must not be blank";
+    public static final String CONTACT_INVALID = "Invalid contact number";
+}
